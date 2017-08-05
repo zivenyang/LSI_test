f181b5f69dd8211af6eb587bbedfae0dcce14e65
zivenyang <yangziwen1994@gmail.com>
2017-08-05 09:52:31 +0800

    update svd test

--- svd_test.py
+++ svd_test.py
@@ -4,58 +4,36 @@ import numpy as np
 
 
 def svd(A): # 奇异值分解
-    AAT = np.matmul(A, A.T)
-    eigen_values2, eigen_vectors2 = np.linalg.eig(AAT)
-
     ATA = np.matmul(A.T, A)
-    eigen_values, eigen_vectors = np.linalg.eig(ATA)
-    print(u"特征值=" , eigen_values)
-    print(u"特征向量=", eigen_vectors)
-    print(u"特征值2=" , eigen_values2)
-    print(u"特征向量2=", eigen_vectors2)
+    eigen_values, eigen_vector = np.linalg.eig(ATA)
 
-    U = eigen_vectors2
-    V = eigen_vectors.T
-    for i in range(eigen_values2.size):
-        if eigen_values2[i] > 0:
-            eigen_values2[i] = np.sqrt(eigen_values2[i])
-        else:
-            eigen_values2[i] = 0
+    sigma = np.sqrt(eigen_values)
+    sort_id = np.argsort(-sigma)
+    sigma = sigma[sort_id]
+    V = eigen_vector[:, sort_id]
+    VT = V.T
+    S = np.diag(sigma)
+    _S = np.linalg.inv(S)
+
+    U = np.matmul(np.matmul(A, V), _S)
+
+    result = np.matmul(np.matmul(U, S), VT)
+    print('U=\n', U)
+    print('\nS=\n', S)
+    print('\nV=\n', V)
+    print('\nA=\n', result)
+    print('\nVT=\n', VT)
+
+    return U, sigma, VT
 
 
-    S = np.zeros([eigen_values2.shape[0], eigen_values2.shape[0]])
-    for i in range(eigen_values2.shape[0]):
-        if eigen_values2[i] is not 0:
-            S[i][i] = eigen_values2[i]
-    s = S[:U.shape[0], :V.shape[0]]
-
-    result = np.matmul(np.matmul(U, s), V)
-
-    print('U=', U)
-    print('S=', s)
-    print('V=', V.T)
-    print('A=', result)
-    # sort_id = np.argsort(-sigma)
-    # new_sigma = sigma[sort_id]
-    # print('new_sigma=', new_sigma)
-
-    # new_V = V[:, sort_id]
-    # print('new_V=', new_V)
-
-    # A_V = np.matmul(A, new_V)
-    # print(A_V)
-    # U = np.matmul(A_V, np.linalg.inv(np.diag(new_sigma)))
-
-    # print('U=', U)
-
-    # result = np.matmul(np.matmul(U, np.diag(new_sigma)), new_V.T)
-    # print('result=',result)
-    # S = new_sigma
-    # V = new_V
-
-    return U, S, V
 
 # A = np.array([[5, 5],[-1, 7]])
 A = np.array([[1, 1], [1, 1], [0,0]])
+# A = np.array([[4, 0], [3, -5]])
 if __name__ == '__main__':
     svd(A)
+    [U, S, V] = np.linalg.svd(A)
+    print('\n_U=\n', U)
+    print('\n_S=\n', S)
+    print('\n_V=\n', V)
